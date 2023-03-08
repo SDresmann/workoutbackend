c0dfa830942a47ed95771643897a554674e81012
Spencer Dresmann <sdresmann@thekablegroup.com>
2023-03-08 15:59:29 -0500

    letsgo

--- server.js
+++ server.js
@@ -4,18 +4,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
+const cors =require('cors')
 
 // express app
 const app = express()
 
 // middleware
 app.use(express.json())
-
+app.use(cors())
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
-
+app.get('/', (req,res)=>{
+  res.setHeader("Access-Control-Allow-Credentials","true");
+})
 // routes
 app.use("/api/workouts",workoutRoutes)
 app.use("/api/user",userRoutes)
